b8330ceda60bea316d4209f4b556293374a05674
Artem Kozlov <artemix9@gmail.com>
2018-06-23 17:07:35 +0300

    Fix Workspace

--- src/components/Workspace.vue
+++ src/components/Workspace.vue
@@ -2,19 +2,29 @@
   <div>
     <div id="nav">
       <ul class="nav nav-pills">
+        <li class="nav-item">
+          <router-link
+            to="/"
+            class="nav-link"
+            :class="{ active: !$route.params.deviceId }">
+            Home
+          </router-link>
+        </li>
         <li
           class="nav-item"
           v-for="(device, index) in devices"
           :key="index">
           <router-link
             :to="'/' + index"
-            :class="{ 'nav-link': true, active: index === parseInt($route.params.deviceId) }">
+            class="nav-link"
+            :class="{ active: index === parseInt($route.params.deviceId) }">
             Device {{ index }}
           </router-link>
         </li>
       </ul>
     </div>
-    <h2>Device {{ $route.params.deviceId }}</h2>
+    <h2 v-if="$route.params.deviceId">Device {{ $route.params.deviceId }}</h2>
+    <h2 v-else>Add or choose a device</h2>
   </div>
 </template>
 
